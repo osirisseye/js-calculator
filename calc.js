f5b25161ba25c716d61606b1731f070a1bd04824
Lukasz <lukaszandruchow@gmail.com>
2017-06-13 17:44:57 +0100

    Basic functionality of a button
    Button displays on the screen its value;
    First step to build equation and later solve it.

--- calc.js
+++ calc.js
@@ -1,9 +1,20 @@
-
+window.onload = function() {
 //function has to 'build' equation based on the user's input
 
-function click(){
-	var equation = ;
-	
+var fulleq = "";
+
+document.getElementById("9").onclick = function fun() {
+      document.getElementById("eq").innerHTML = document.getElementById("9").value;
+        
+        
+    }
+//function click(){
+//	document.getElementById("body").innerHTML = "pusto";
+//	}
+
 }
 
-document.getElementById("quoteplace").innerHTML = quot;
+
+
+//document.getElementById("quoteplace").innerHTML = quot;
+//document.getElementById(id).value;
